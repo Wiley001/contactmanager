2ab46c7e4ff8939a8cbd58a5b0b76d9fe4fe6988
James Day <jamesd@wileycom.com>
2018-10-17 10:30:10 -0500

    Updates

---
+++ precache-manifest.7a503bac6770bb7b67c01c83f0aacc0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "c85e33fca0ce7d06d10d",
+    "url": "/contactmanager/static/js/main.c85e33fc.chunk.js"
+  },
+  {
+    "revision": "6a3c927b2440b419ec73",
+    "url": "/contactmanager/static/js/1.6a3c927b.chunk.js"
+  },
+  {
+    "revision": "c85e33fca0ce7d06d10d",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "6a3c927b2440b419ec73",
+    "url": "/contactmanager/static/css/1.9c27f035.chunk.css"
+  },
+  {
+    "revision": "68b562e241d6349a0dad859a5fc0a900",
+    "url": "/contactmanager/index.html"
+  }
+];
